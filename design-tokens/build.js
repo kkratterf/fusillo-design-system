c0e5bcc3dd435fef4b51a21cd16a8b078f53792f
kkratterf <kkratterf@gmail.com>
2023-09-24 18:38:31 +0200

    Settings tokens transform

---
+++ design-tokens/build.js
@@ -0,0 +1,18 @@
+const StyleDictionary = require('style-dictionary').extend({
+  source: ['properties/**/*.json'],
+  platforms: {
+    scss: {
+      transformGroup: 'scss',
+      buildPath: 'build/',
+      files: [
+        {
+          destination: 'variables.scss',
+          format: 'scss/variables',
+        },
+      ],
+    },
+    // ...
+  },
+});
+
+StyleDictionary.buildAllPlatforms();
