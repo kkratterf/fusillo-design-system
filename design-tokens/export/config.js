d82061ba52357bebad3d825d37adb8a459d61681
kkratterf <kkratterf@gmail.com>
2023-09-29 09:47:27 +0000

    Token Update

--- design-tokens/export/config.js
+++ design-tokens/export/config.js
@@ -1,11 +1,9 @@
 /**
  * Do not edit directly
- * Generated on Fri, 29 Sep 2023 08:50:34 GMT
+ * Generated on Fri, 29 Sep 2023 09:47:27 GMT
  */
 
 module.exports = {
   "ConfigName": "Fusillo Design System",
-  "ConfigVersion": "Version 1.0.1",
-  "ConfigLatestUpdate": "04/09/2023",
-  "ConfigLicence": "MIT Licence"
+  "ConfigVersion": "1.0.1"
 };
