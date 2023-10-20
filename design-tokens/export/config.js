b408f7347e263aa3a576247ec34297cffd7c56c4
kkratterf <kkratterf@gmail.com>
2023-10-20 08:46:38 +0000

    Token Update

--- design-tokens/export/config.js
+++ design-tokens/export/config.js
@@ -1,10 +1,10 @@
 /**
  * Do not edit directly
- * Generated on Fri, 20 Oct 2023 08:28:41 GMT
+ * Generated on Fri, 20 Oct 2023 08:46:38 GMT
  */
 
 module.exports = {
-  "ConfigName": "Wow",
+  "ConfigName": "Fusillo Design System",
   "ConfigVersion": "1.0.1",
   "ConfigBrand": "Al dente"
 };
