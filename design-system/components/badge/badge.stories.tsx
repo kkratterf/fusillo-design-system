c565505fa6aa3fc51e856785b9b5e6b92e5649a2
kkratterf <kkratterf@gmail.com>
2023-10-22 19:27:13 +0200

    New Tag Component & Components Fix

---
+++ design-system/components/badge/badge.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Badge } from '.';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof Badge> = {
+  title: 'Design System/Components/Badge',
+  component: Badge,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+    controls: {
+      exclude: ['theme'],
+    },
+    docs: {
+      description: {
+        component: 'Displays a callout for user attention.',
+      },
+    },
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    // backgroundColor: { control: 'color' },
+  },
+  args: {
+    title: 'Button',
+    variant: 'default',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Badge>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+
+export const Default = {
+  args: {
+    variant: 'default',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Cool description bro',
+      },
+    },
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/LKQ4FJ4bTnCSjedbRpk931/Sample-File',
+    },
+  },
+};
+
+export const Danger: Story = {
+  args: {
+    variant: 'danger',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Cool description bro',
+      },
+    },
+  },
+};
