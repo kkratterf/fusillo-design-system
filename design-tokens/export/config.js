8087aa09ab0b536172656c6b0462e29872f0e277
kkratterf <kkratterf@gmail.com>
2023-10-26 19:12:18 +0000

    Token Update

--- design-tokens/export/config.js
+++ design-tokens/export/config.js
@@ -1,10 +1,10 @@
 /**
  * Do not edit directly
- * Generated on Thu, 26 Oct 2023 18:41:25 GMT
+ * Generated on Thu, 26 Oct 2023 19:12:18 GMT
  */
 
 module.exports = {
-  "ConfigName": "Demo Sprintaly",
+  "ConfigName": "Fusillo Design System",
   "ConfigVersion": "1.0.1",
   "ConfigBrand": "Al dente"
 };
