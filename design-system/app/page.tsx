f1442a0d9292192b7d0cff79adb907b358a86a72
kkratterf <kkratterf@gmail.com>
2023-12-22 12:01:08 +0100

    fix build

--- design-system/app/page.tsx
+++ design-system/app/page.tsx
@@ -290,7 +290,7 @@ export default function Home() {
             <DialogHeader>
               <DialogTitle>Edit profile</DialogTitle>
               <DialogDescription>
-                Make changes to your profile here. Click save when you're done.
+                Make changes to your profile here. Click save when you re done.
               </DialogDescription>
             </DialogHeader>
             <div className="grid gap-4 py-4">
