a0bbe6f94cf01ef46ee9bfea39de3c86045769df
kkratterf <kkratterf@gmail.com>
2023-12-30 18:40:23 +0100

    fix docs

--- documentation/components/components-accordion.tsx
+++ documentation/components/components-accordion.tsx
@@ -1,6 +1,5 @@
 // Import core
 // Import customs
-import { AccessibilityTable } from '@/docs/components/accessibility-table.mdx';
 import Preview from '@/docs/components/preview';
 import {
   Accordion,
@@ -133,77 +132,3 @@ export const AccordionDefaultValue = () => (
     </Accordion>
   </Preview>
 );
-
-export const AccordionAccessibility = () => {
-  const accordionAccessibilityData = [
-    {
-      key: 'Space',
-      description: (
-        <>
-          When focus is on an{' '}
-          <code className="cell-accessibility-code">AccordionTrigger</code> of a
-          collapsed section, expands the section.
-        </>
-      ),
-    },
-    {
-      key: 'Enter',
-      description: (
-        <>
-          When focus is on an{' '}
-          <code className="cell-accessibility-code">AccordionTrigger</code> of a
-          collapsed section, expands the section.
-        </>
-      ),
-    },
-    {
-      key: 'Tab',
-      description: 'Moves focus to the next focusable element.',
-    },
-    {
-      key: 'Shift+Tab',
-      description: 'Moves focus to the previous focusable element.',
-    },
-    {
-      key: 'ArrowDown',
-      description: (
-        <>
-          Moves focus to the next{' '}
-          <code className="cell-accessibility-code">AccordionTrigger</code>
-        </>
-      ),
-    },
-    {
-      key: 'ArrowUp',
-      description: (
-        <>
-          Moves focus to the previous{' '}
-          <code className="cell-accessibility-code">AccordionTrigger</code>
-        </>
-      ),
-    },
-    {
-      key: 'Home',
-      description: (
-        <>
-          When focus is on an{' '}
-          <code className="cell-accessibility-code">AccordionTrigger</code>,
-          moves focus to the first{' '}
-          <code className="cell-accessibility-code">AccordionTrigger</code>
-        </>
-      ),
-    },
-    {
-      key: 'End',
-      description: (
-        <>
-          When focus is on an{' '}
-          <code className="cell-accessibility-code">AccordionTrigger</code>,
-          moves focus to the last{' '}
-          <code className="cell-accessibility-code">AccordionTrigger</code>
-        </>
-      ),
-    },
-  ];
-  return <AccessibilityTable rows={accordionAccessibilityData} />;
-};
