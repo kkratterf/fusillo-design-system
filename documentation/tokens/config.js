6a6d5b6bae0ff424e27ca5a2d495138b1b7e6e12
kkratterf <kkratterf@gmail.com>
2024-01-03 16:30:35 +0000

    Token Update

--- documentation/tokens/config.js
+++ documentation/tokens/config.js
@@ -1,10 +1,10 @@
 /**
  * Do not edit directly
- * Generated on Wed, 03 Jan 2024 16:00:05 GMT
+ * Generated on Wed, 03 Jan 2024 16:30:35 GMT
  */
 
 module.exports = {
-  "ConfigName": "Test ds",
+  "ConfigName": "Fusillo Design System",
   "ConfigVersion": "1.0.1",
   "ConfigBrand": "Al dente"
 };
--- documentation/tokens/foundations.js
+++ documentation/tokens/foundations.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Wed, 03 Jan 2024 16:00:04 GMT
+ * Generated on Wed, 03 Jan 2024 16:30:35 GMT
  */
 
 module.exports = {
@@ -164,7 +164,7 @@ module.exports = {
         },
         "brand-500": {
           "type": "color",
-          "value": "#84cc16",
+          "value": "#3b82f6",
           "description": "",
           "$extensions": {
             "variableId": "VariableID:2:48"
@@ -173,7 +173,7 @@ module.exports = {
           "isSource": true,
           "original": {
             "type": "color",
-            "value": "{Foundations.Color.Lime.lime-500}",
+            "value": "{Foundations.Color.Blue.blue-500}",
             "description": "",
             "$extensions": {
               "variableId": "VariableID:2:48"
@@ -195,7 +195,7 @@ module.exports = {
         },
         "brand-600": {
           "type": "color",
-          "value": "#65a30d",
+          "value": "#2563eb",
           "description": "",
           "$extensions": {
             "variableId": "VariableID:2:49"
@@ -204,7 +204,7 @@ module.exports = {
           "isSource": true,
           "original": {
             "type": "color",
-            "value": "{Foundations.Color.Lime.lime-600}",
+            "value": "{Foundations.Color.Blue.blue-600}",
             "description": "",
             "$extensions": {
               "variableId": "VariableID:2:49"
