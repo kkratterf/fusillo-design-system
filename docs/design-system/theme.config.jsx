1fcd30b792d33920ff114bafec4c7a43ee3ab3a3
kkratterf <kkratterf@gmail.com>
2024-02-13 14:35:47 +0100

    New favicon / docs text / new homepage

--- docs/design-system/theme.config.jsx
+++ docs/design-system/theme.config.jsx
@@ -1,19 +1,100 @@
 import * as ds from '@docs/tokens/config.js';
-
-const EmptyComponent = () => {
-  return <></>;
-};
+import EmptyComponent from '@docs/components/config/empty-component'
+import Logo from '@docs/components/config/logo';
 
 export default {
-  logo: <span className="docs_logo">{ds.ConfigName}</span>,
+  logo: (
+    <span className="docs_logo flex items-center gap-2 bg-danger">
+      <Logo />
+      {ds.ConfigName}
+    </span>
+  ),
   project: {
-    link: 'https://github.com/kkratterf',
+    link: 'https://github.com/kkratterf/fusillo',
   },
   useNextSeoProps() {
     return {
       titleTemplate: `${ds.ConfigName} – %s`,
     };
   },
+  head: (
+    <>
+      <link
+        rel="apple-touch-icon"
+        sizes="57x57"
+        href="/favicon/apple-icon-57x57.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="60x60"
+        href="/favicon/apple-icon-60x60.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="72x72"
+        href="/favicon/apple-icon-72x72.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/favicon/apple-icon-76x76.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/favicon/apple-icon-114x114.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/favicon/apple-icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/favicon/apple-icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/favicon/apple-icon-152x152.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/favicon/android-icon-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/favicon/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicon/manifest.json" />
+      <meta
+        name="msapplication-TileImage"
+        content="/favicon/ms-icon-144x144.png"
+      />
+    </>
+  ),
   footer: {
     component: null,
   },
