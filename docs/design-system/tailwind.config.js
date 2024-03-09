57964aede6eaf0197a195ca8f80cd4be826a98ad
kkratterf <kkratterf@gmail.com>
2024-03-09 15:21:07 +0100

    fix build

--- docs/design-system/tailwind.config.js
+++ docs/design-system/tailwind.config.js
@@ -51,10 +51,10 @@ module.exports = {
   },
   plugins: [
     require('tailwindcss-animate'),
-    function ({ matchUtilities, theme }: any) {
+    function ({ matchUtilities, theme }) {
       matchUtilities(
         {
-          'bg-grid': (value: any) => ({
+          'bg-grid': (value) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="40" height="40" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
